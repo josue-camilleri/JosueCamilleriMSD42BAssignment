fc9546b7dac23df9cf584677a001b836e205c423
josue-camilleri <73096448+josue-camilleri@users.noreply.github.com>
2021-01-06 00:15:46 +0100

    2D Car Game
    Task 1a-j

---
+++ HomeAssignment/JosueCamilleri42BGame/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    //a list of WaveConfigs
+    [SerializeField] List<WaveConfig> waveConfigList;
+
+    //start from wave 0
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //setting the current wave to wave 1
+        var currentWave = waveConfigList[startingWave];
+
+        //starts coroutine that spawn all obstacles in currentWave
+        StartCoroutine(SpawnAllObstaclesInWave(currentWave));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //specifying the wave that need to spawn the obstacle.
+    private IEnumerator SpawnAllObstaclesInWave(WaveConfig waveConfig)
+    {
+        //spawn the enemy prefab from waveToSpawn at the position of the first waypoint in Path
+        Instantiate(waveConfig.GetObstaclePrefab(),
+            waveConfig.GetWaypointsList()[0].transform.position,
+            Quaternion.identity);
+
+        //wait timeBetweenSpawn before spawning another obstacle
+        yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+    }
+}
---
+++ HomeAssignment/JosueCamilleri42BGame/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //how fast the player will move horizontaly
+    float moveSpeed = 7f;
+
+    //used for SetUpMoveBoundaries()
+    float xMin, xMax, yMin, yMax;
+
+    float padding = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //setting up the boundaries according to the camera
+    private void SetUpMoveBoundaries()
+    {
+        //getting the camera from unity
+        Camera gameCamera = Camera.main;
+
+        //xMin = 0, xMax = 1
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        //yMin = 0, yMax = 1
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        //newXPosition = current position in x + difference moved in x-axis
+        var newXPos = transform.position.x + deltaX;
+
+        //claml the value of newXPos between xMin (0) and xMax(1) in camera view
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        //move the player ship on the x- and y-axis
+        transform.position = new Vector2(newXPos, -4);
+    }
+}
